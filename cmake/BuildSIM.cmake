4a1b2c69210f01430bd6830d54522529e0275a6c
Dawson <horvath.dawson@gmail.com>
2021-12-04 03:46:55 -0800

    Auto fixes pip install :)

--- cmake/BuildSIM.cmake
+++ cmake/BuildSIM.cmake
@@ -12,3 +12,20 @@ ycm_ep_helper(SIM TYPE GIT
               CMAKE_ARGS -DSCENARIO_ENABLE_BINDINGS:BOOL=${BAESIANBALANCER_USES_PYTHON}
                          -DBINDINGS_INSTALL_PREFIX:PATH=${BAESIANBALANCER_SUPERBUILD_PYTHON_INSTALL_DIR}
               DEPENDS gym-ignition)
+
+
+set(EASY_INSTALL_PATH
+    "${YCM_EP_INSTALL_DIR}/${BAESIANBALANCER_SUPERBUILD_PYTHON_INSTALL_DIR_SETUP_SH}")
+message(STATUS "${Python3_EXECUTABLE} setup.py egg_info --egg-base=${EASY_INSTALL_PATH}")
+
+add_custom_command(TARGET SIM POST_BUILD
+    COMMAND ${Python3_EXECUTABLE} setup.py egg_info --egg-base=${EASY_INSTALL_PATH}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/gym-ignition/scenario
+    COMMENT "Installing egg files for scenario..."
+)
+
+add_custom_command(TARGET SIM POST_BUILD
+    COMMAND ${Python3_EXECUTABLE} setup.py egg_info --egg-base=${EASY_INSTALL_PATH}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/iDynTree
+    COMMENT "Installing egg files for idyntree..."
+)
