7a64daf47ed698a46295ea237d9c2ff9b2073827
Dawson <horvath.dawson@gmail.com>
2021-12-28 20:17:10 -0800

    changed the gym_ignition install to actually link properly. VERBATIM COMMAND is required for add_custom_command.

--- cmake/Buildgym-ignition.cmake
+++ cmake/Buildgym-ignition.cmake
@@ -30,16 +30,27 @@ if(BAESIANBALANCER_USES_PYTHON)
       COMMENT "Installing egg files for scenario..."
   )
 
+
+  # add_custom_command(TARGET gym-ignition POST_BUILD
+  #                   COMMAND ${CMAKE_COMMAND} -E copy_directory
+  #                   ${PROJECT_SOURCE_DIR}/src/gym-ignition/python/gym_ignition ${EGG_BASE_PATH_SCENARIO}/gym_ignition
+  #                   COMMENT "Moving gym-ignition into build python directory ${EGG_BASE_PATH_SCENARIO}...")
+  # add_custom_command(TARGET gym-ignition POST_BUILD
+  #   COMMAND ${Python3_EXECUTABLE} setup.py egg_info --egg-base=${EGG_BASE_PATH_SCENARIO}
+  #   # COMMAND ${Python3_EXECUTABLE} -m pip install -e .
+  #   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/gym-ignition
+  #   COMMENT "Installing egg files for gym-ignition..."
+  # )
+
+  set(gym_ignition_install_dir ${EGG_BASE_PATH_SCENARIO}/gym_ignition)
   add_custom_command(TARGET gym-ignition POST_BUILD
-                     COMMAND ${CMAKE_COMMAND} -E copy_directory
-                     ${PROJECT_SOURCE_DIR}/src/gym-ignition ${EGG_BASE_PATH_SCENARIO}/gym_ignition
-                     COMMENT "Moving gym-ignition into build python directory ${EGG_BASE_PATH_SCENARIO}...")
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/src/gym-ignition ${gym_ignition_install_dir}
+    COMMENT "Moving gym-ignition into build python directory ${gym_ignition_install_dir}...")
 
   add_custom_command(TARGET gym-ignition POST_BUILD
-    COMMAND ${Python3_EXECUTABLE} setup.py egg_info --egg-base=${EGG_BASE_PATH_SCENARIO}
-    # COMMAND ${Python3_EXECUTABLE} -m pip install -e .
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/src/gym-ignition
-    COMMENT "Installing egg files for gym-ignition..."
+    VERBATIM COMMAND ${Python3_EXECUTABLE} -c "import setuptools;import site;import sys;site.ENABLE_USER_SITE = 1;sys.argv[1:]=[\"develop\",\"--user\"];setuptools.setup()"
+    WORKING_DIRECTORY ${gym_ignition_install_dir}
+    COMMENT "Installing egg files link for gym-ignition..."
   )
 
 endif()
