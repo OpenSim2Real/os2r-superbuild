ae1e1d3d03f6ace24653d2e5d8718f79d439be60
Dawson Horvath <horvath.dawson@gmail.com>
2022-01-07 23:52:10 -0800

    fixed scenario_monopod depend

--- cmake/Buildscenario_monopod.cmake
+++ cmake/Buildscenario_monopod.cmake
@@ -2,6 +2,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(gym-ignition QUIET)
+find_or_build_package(monopod_sdk QUIET)
 
 ycm_ep_helper(scenario_monopod TYPE GIT
               STYLE GITHUB
@@ -13,4 +14,4 @@ ycm_ep_helper(scenario_monopod TYPE GIT
                          -DBINDINGS_INSTALL_PREFIX:PATH=${OPENSIM2REAL_SUPERBUILD_PYTHON_INSTALL_DIR}
                          -DBUILD_DOCS:BOOL=${OPENSIM2REAL_ENABLE_DOC_COMPILATION}
                          -DBUILD_DOCS_SUPERBUILD:BOOL=${OPENSIM2REAL_ENABLE_DOC_COMPILATION}
-              DEPENDS gym-ignition)
+              DEPENDS gym-ignition monopod_sdk)
