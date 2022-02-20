a35755cee40c3ea06f0014c74f1ba066a4f45d67
Dawson Horvath <horvath.dawson@gmail.com>
2022-02-20 02:12:08 -0800

    fixed time series depend for monopod sdk

--- cmake/Buildmonopod_sdk.cmake
+++ cmake/Buildmonopod_sdk.cmake
@@ -3,6 +3,7 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(real_time_tools QUIET)
+find_or_build_package(time_series QUIET)
 
 ycm_ep_helper(monopod_sdk TYPE GIT
               STYLE GITHUB
@@ -12,4 +13,4 @@ ycm_ep_helper(monopod_sdk TYPE GIT
               FOLDER src
               CMAKE_ARGS -DBUILD_DOCS:BOOL=${OPENSIM2REAL_ENABLE_BUILDDOCS}
                          -DBUILD_DOCS_SUPERBUILD:BOOL=${OPENSIM2REAL_ENABLE_BUILDDOCS}
-              DEPENDS real_time_tools)
+              DEPENDS real_time_tools, time_series)
